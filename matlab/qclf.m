4275170b0c12ed784c1caa3034f393286a3203e8
Daniel Wagenaar <daw@caltech.edu>
2013-12-12 21:22:06 -0500

    Renamed clq to qclf but added alias

---
+++ matlab/qclf.m
@@ -0,0 +1,19 @@
+function qclf 
+% QCLF - Clear current QPlot figure
+%   QCLF clears the current QPlot figure. 
+global qp_data
+fd = qp_fd(1);
+fn = qp_data.curfn;
+fclose(fd);
+fd = fopen(fn, 'r');
+l0 = fgets(fd);
+fclose(fd);
+fd = fopen(fn, 'w');
+fprintf(fd, '%s', l0);
+idx = strmatch(fn, qp_data.fns, 'exact');
+qp_data.info(idx).fd = fd;
+
+qp_reset(idx);
+
+qp_flush(fd);
+
