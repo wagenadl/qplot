42cc3820ff4adf179fd0e1b1dcbf61359cb2c377
Daniel A. Wagenaar <daw@caltech.edu>
2019-07-04 09:12:34 -0700

    lut works

--- doc/pyref/lut_eg.py
+++ doc/pyref/lut_eg.py
@@ -1,30 +1,31 @@
 import pyqplot as qp
 import numpy as np
 import matplotlib.pyplot as plt
+import cv2
 
 qp.figure('lut', 3, 3)
 
 xx = np.linspace(0, 2*np.pi, 101)
 xx = np.tile(xx, (len(xx), 1))
 yy = np.transpose(xx)
-zz = -xx #np.cos(xx)+np.sin(yy)
+zz = np.cos(xx)+np.sin(yy)
 
 qp.imsc(zz, [0, 0, 1, 1])
 qp.cbar()
-qp.shrink()
-'''
-qp.lut(plt.cm.spectral(np.arange(plt.cm.spectral.N)))
 
+c = plt.cm.get_cmap('rainbow')
+qp.lut(c(range(c.N)))
 qp.imsc(zz, [0, 1.1, 1, 1])
 qp.cbar()
 
-qp.lut(plt.cm.datad['bwr'])
-
+c = plt.cm.get_cmap('seismic')
+qp.lut(c(range(c.N)))
 qp.imsc(zz, [1.5, 0, 1, 1])
 qp.cbar()
 
-qp.lut(plt.cm.datad['hot'])
+c = plt.cm.get_cmap('YlOrRd')
+qp.lut(c(range(c.N)))
 qp.imsc(zz, [1.5, 1.1, 1, 1])
 qp.cbar()
 
-'''
+
