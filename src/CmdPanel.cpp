700a7db71c6a5067872d4f32b47d9d5e51d13547
Daniel A. Wagenaar <daw@caltech.edu>
2023-12-12 19:44:13 -0800

    PANEL no longer draws outline

--- src/CmdPanel.cpp
+++ src/CmdPanel.cpp
@@ -31,18 +31,17 @@ bool CmdPanel::usage() {
 bool CmdPanel::parse(Statement const &s) {
   if (s.length()==2 && s[1].typ==Token::DASH)
     return true;
-  else if (s.length()==2 &&
+  if (s.length()==2 &&
 	   (s[1].typ==Token::CAPITAL || s[1].typ==Token::BAREWORD))
     return true;
-  else if (s.length()==2+4 &&
+  if (s.length()==2+4 &&
 	   (s[1].typ==Token::CAPITAL || s[1].typ==Token::BAREWORD) &&
 	   s[2].typ==Token::NUMBER &&
 	   s[3].typ==Token::NUMBER &&
 	   s[4].typ==Token::NUMBER &&
 	   s[5].typ==Token::NUMBER)
     return true;
-  else
-    return usage();
+  return usage();
 }
 
 
@@ -50,16 +49,14 @@ void CmdPanel::render(Statement const &s, Figure &f, bool dryrun) {
   f.leavePanel();
   if (s[1].typ==Token::DASH)
     return;
-  
-  QRectF area;
-  if (s.length()==6) {
-    area = QRectF(pt2iu(s[2].num), pt2iu(s[3].num),
-		  pt2iu(s[4].num), pt2iu(s[5].num));
-    if (!dryrun)
-      f.painter().drawRect(area);
-  }
   f.choosePanel(s[1].str);
+  if (s.length()<6)
+    return;
+  
+  QRectF area(pt2iu(s[2].num), pt2iu(s[3].num),
+              pt2iu(s[4].num), pt2iu(s[5].num));
+  //if (!dryrun)
+  //  f.painter().drawRect(area);
   qDebug() << "panel" << s[1].str << f.extent() << area;
-  if (s.length()==6)
-    f.setExtent(area);
+  f.setExtent(area);
 }
