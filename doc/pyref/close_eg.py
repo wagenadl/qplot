3b179a602fe44ccd4135cbb6d98384179a619cff
Daniel A. Wagenaar <daw@caltech.edu>
2023-12-27 11:30:47 -0800

    Improved docs

--- doc/pyref/close_eg.py
+++ doc/pyref/close_eg.py
@@ -3,5 +3,7 @@ import numpy as np
 import time
 
 qp.figure('', 3, 3)
+qp.plot(np.arange(0, 10, .1), np.cos(np.arange(0, 10, .1)))
 time.sleep(1)
+
 qp.close()
--- doc/pyref/figure_eg.py
+++ doc/pyref/figure_eg.py
@@ -2,3 +2,6 @@ import qplot as qp
 import numpy as np
 
 qp.figure('figure', 3, 3)
+
+qp.plot(np.arange(0, 10, .1), np.cos(np.arange(0, 10, .1)))
+
