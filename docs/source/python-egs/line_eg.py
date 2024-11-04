d22b15cdca86c3b266459bcc18b754064168ee2a
Daniel A. Wagenaar <daw@caltech.edu>
2024-11-03 22:29:15 -0800

    bugfix to line_eg

--- docs/source/python-egs/line_eg.py
+++ docs/source/python-egs/line_eg.py
@@ -8,6 +8,9 @@ yy = np.sin(xx)
 
 qp.plot(xx, yy)
 
+dx = np.diff(xx)
+dy = np.diff(yy)
+
 for n in range(2, len(xx)-1, 5):
-    qp.at(xx[n], yy[n], xx[n+1]-xx[n], yy[n+1]-yy[n])
+    qp.at(xx[n], yy[n], along=(dx[n], dy[n]))
     qp.line([0, 0],[-5, 5])
