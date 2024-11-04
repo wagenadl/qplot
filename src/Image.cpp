141ee0fa8a7e48b26008f9f292e312148f5a76c9
Daniel A. Wagenaar <daw@caltech.edu>
2024-11-03 22:30:02 -0800

    Image.cpp updated to modern standard

--- src/Image.cpp
+++ src/Image.cpp
@@ -1,13 +1,15 @@
 // Image.cpp
 
 #include "Image.h"
+#include <QDebug>
 
 namespace Image {
   QImage build(int X, int Y, int C,
                QVector<double> const &cdata) {
+    Q_ASSERT(X*Y*C == cdata.size());
     QImage img = QImage(X, Y, QImage::Format_ARGB32);
     uchar *dst = img.bits();
-    QVector<double>::const_iterator src = cdata.begin();
+    double const *src = cdata.data();
     switch (C) {
     case 1:
       for (int y=0; y<Y; y++) {
@@ -41,21 +43,21 @@ namespace Image {
             *dst++ = (v<0) ? 0 : (v>1) ? 255 : int(v * 255.999);
           }
           *dst++ = 255;
-          src+=3;
+          src += 3;
         }
       }
       break;
     case 4:
       for (int y=0; y<Y; y++) {
         for (int x=0; x<X; x++) {
-          double const *s1 = src+2;
+          double const *s1 = src + 2;
           for (int c=0; c<3; c++) {
             double v = *s1--;
             *dst++ = (v<0) ? 0 : (v>1) ? 255 : int(v * 255.999);
           }
           double v = src[3];
           *dst++ = (v<0) ? 0 : (v>1) ? 255 : int(v * 255.999);
-          src+=4;
+          src += 4;
         }
       }
     }
